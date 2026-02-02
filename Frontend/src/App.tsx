cc2e1ad453e3ee14b21442c724aeee9a120e0bcd
Nivesh Shrestha <nivesh.shrestha.94@gmail.com>
2026-02-02 15:19:43 +0545

    feat: add routing with react-router-dom and create Header component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './UserPages/Home';
 
 const App = () => {
   return (
-    <div>
+  <BrowserRouter>
+  <Routes>
 
-    </div>
+    {/* User Pages */}
+    <Route path='/' element={<Home />} />
+
+  </Routes>
+  </BrowserRouter>
   );
 };
 
---
+++ Frontend/src/UserComponent/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-red-600'>This is header</div>
+  )
+}
+
+export default Header
