73186b1ae5b43a2b1cc25b47d6f93acc72e3e75f
Nivesh Shrestha <nivesh.shrestha.94@gmail.com>
2026-02-12 21:20:29 +0545

    added footer to home page

--- Frontend/src/UserPages/Home.tsx
+++ Frontend/src/UserPages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../UserComponent/Header'
+import Footer from '../UserComponent/Footer'
 
 const Home = () => {
   return (
     <div>
         <Header />
+        <Footer />
     </div>
   )
 }
