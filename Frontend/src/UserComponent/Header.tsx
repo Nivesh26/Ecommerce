5a1d6fcec3665abf7935881bc43f63e0327e9404
Nivesh Shrestha <nivesh.shrestha.94@gmail.com>
2026-04-03 22:08:20 +0545

    fix: restore original header text

--- Frontend/src/UserComponent/Header.tsx
+++ Frontend/src/UserComponent/Header.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-red-600'>this is the header</div>
-  
+    <div className='bg-red-600'>Header</div>
   )
-}w
-
+}
 export default Header
