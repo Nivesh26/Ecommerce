bec401d5f3fbe3349cd2ac2adcbff326f8d38802
Nivesh Shrestha <nivesh.shrestha.94@gmail.com>
2026-04-29 21:05:44 +0545

    header change

--- Frontend/src/UserComponent/Header.tsx
+++ Frontend/src/UserComponent/Header.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-red-600'>This is Header</div>
+    <div className='bg-red-600'>This is header
+    </div>
   )
 }
 export default Header
