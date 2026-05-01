3b46b3ed1488b8d66467e754da78a8f3f891ac6c
Nivesh Shrestha <nivesh.shrestha.94@gmail.com>
2026-05-02 01:06:23 +0545

    fix: correct footer text formatting

--- Frontend/src/UserComponent/Footer.tsx
+++ Frontend/src/UserComponent/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-red-600'> this is footer.</div>
+    <div className='bg-red-600'> this is footer</div>
   )
 }
 
