61f6bfcc52526b6a4cea2794802de134b74bf8a7
Nivesh Shrestha <nivesh.shrestha.94@gmail.com>
2026-06-14 22:35:36 +0545

    fix: correct typo in Header component text

--- Frontend/src/UserComponent/Header.tsx
+++ Frontend/src/UserComponent/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-red-600'>This is hdader 
+    <div className='bg-red-600'>This is Header Component 
     </div>
   )
 }
