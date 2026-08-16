d5dcbddb1e4cf8826e8315ccd88277ca1d953fc6
Nivesh Shrestha <nivesh.shrestha.94@gmail.com>
2026-08-16 20:27:48 +0545

    fix: import and render CTA component in Home page

--- Frontend/src/UserPages/Home.tsx
+++ Frontend/src/UserPages/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../UserComponent/Header'
 import Footer from '../UserComponent/Footer'
 import Hero from '../UserComponent/Hero'
+import cta from '../UserComponent/cta'
 
 const Home = () => {
   return (
     <div>
         <Header />
         <Hero />  
+        <cta />
         <Footer />
     </div>
   )
